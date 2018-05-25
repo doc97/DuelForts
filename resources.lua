0c885592135ec9ac5c7a53cc1b65afffc13d879d
doc97 <daniel.riissanen@gmail.com>
2018-05-26 00:03:35 +0300

    Fix formatting in resources.lua

--- resources.lua
+++ resources.lua
@@ -1,14 +1,17 @@
--- Health, Money
 PlayerResources = {
     
-    ["p1Resources"]={
-    ["health"]=35, ["money"]=3, ["shield"]=0, 
-    ["permanents"]={}
+    ["p1Resources"] = {
+        ["health"]  = 35,
+        ["money"]   = 3,
+        ["shield"]  = 0, 
+        ["permanents"] = {}
     },
     
-    ["p2Resources"]={
-    ["health"]=35, ["money"]=4, ["shield"]=0,
-    ["permanents"]={} 
+    ["p2Resources"] = {
+        ["health"]  = 35,
+        ["money"]   = 4,
+        ["shield"]  = 0,
+        ["permanents"] = {} 
     }
     
 }
