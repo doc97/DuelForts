c7073e21fdb44a5be1972e053481a913342086f8
doc97 <daniel.riissanen@gmail.com>
2018-05-26 00:01:21 +0300

    Add tower rendering

--- gamescreen.lua
+++ gamescreen.lua
@@ -1,6 +1,9 @@
+require "resources"
 require "cardpool"
 
 local screen = {}
+local towerHeightPx = love.graphics.getHeight() - 50
+local towerWidthPx = 200
 
 function screen:onEnter()
 
@@ -15,11 +18,30 @@ function screen:update(dt)
 end
 
 function screen:draw()
-    love.graphics.setColor(0, 0, 255)
+    love.graphics.setColor(0.5, 0.5, 0.8)
     love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
 
-    love.graphics.setColor(255, 255, 255)
-    love.graphics.printf("Card pool size: "..Pool[#Pool].base, 0, 200, love.graphics.getWidth(), "center")
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.printf("Card pool size: "..PlayerResources.p1Resources.health, 0, 200, love.graphics.getWidth(), "center")
+
+    -- Draw towers
+    love.graphics.setColor(0, 0, 1)
+    love.graphics.rectangle(
+        "fill",
+        50,
+        towerHeightPx * (100 - PlayerResources.p1Resources.health) / 100,
+        towerWidthPx,
+        towerHeightPx * (PlayerResources.p1Resources.health) / 100
+    )
+
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.rectangle(
+        "fill",
+        love.graphics.getWidth() - 200 - 50,
+        towerHeightPx * (100 - PlayerResources.p2Resources.health) / 100,
+        towerWidthPx,
+        towerHeightPx * (PlayerResources.p2Resources.health) / 100
+    )
 end
 
 function screen:keypressed(key)
