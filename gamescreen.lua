1d7e2686e3c458178c185c6379c11b61c4937ca5
doc97 <daniel.riissanen@gmail.com>
2018-05-26 03:36:41 +0300

    Improve card visuals

--- gamescreen.lua
+++ gamescreen.lua
@@ -7,25 +7,22 @@ local towerHeightPx = love.graphics.getHeight() - 50
 local cardWidthPx = 200
 local cardHeightPx = 300
 local cardSpacePx = 25
+local currentHand = nil
 
 local function cardDrawPlayer1()
-    p1Hand = {}
+    currentHand = {}
     for i = 1, PlayerResources.p1Resources.handsize do
-        local card = Pool[ math.random( #Pool )]
-        table.insert( p1Hand, { base = card.base, index = card.index } )
+        local card = assert(Pool[ math.random( #Pool )])
+        table.insert( currentHand, { base = card.base, index = card.index } )
     end
 end
 
 local function cardDrawPlayer2()
-    p2Hand = {}
-    for i = 1, PlayerResources.p1Resources.handsize do
-        local card = Pool[ math.random( #Pool )]
-        table.insert( p2Hand, { base = card.base, index = card.index } )
+    currentHand = {}
+    for i = 1, PlayerResources.p2Resources.handsize do
+        local card = assert(Pool[ math.random( #Pool )])
+        table.insert( currentHand, { base = card.base, index = card.index } )
     end
-end
-
-function screen:onEnter()
-
 end
 
 local function renderCard(card, x, y)
@@ -35,39 +32,48 @@ local function renderCard(card, x, y)
     love.graphics.setColor(0, 0, 0)
     love.graphics.rectangle("line", x, y, cardWidthPx, cardHeightPx)
     love.graphics.line(x, y + cardHeightPx / 10, x + cardWidthPx, y + cardHeightPx / 10)
+    love.graphics.line(x + cardHeightPx / 10, y, x + cardHeightPx / 10, y + cardHeightPx / 10)
     love.graphics.line(x, y + cardHeightPx / 2, x + cardWidthPx, y + cardHeightPx / 2)
 
-    love.graphics.printf(card.name, x, y + cardHeightPx / 50, cardWidthPx, "center")
+    love.graphics.printf(card and card.name or "???", x, y + cardHeightPx / 40, cardWidthPx, "center")
+    love.graphics.printf(card and card.cost or "?", x, y + cardHeightPx / 40, cardHeightPx / 10, "center")
 end
 
 local function renderCards()
-    local hand = {
-        { name="Card #1" },
-        { name="Card #2" },
-        { name="Card #3" },
-        { name="Card #4" },
-        { name="Card #5" }
-    }
     local hW = love.graphics.getWidth() / 2
     local hH = love.graphics.getHeight() / 2
     local idx = 1
-    if cardHand % 2 == 1 then
-        renderCard(hand[idx], hW - cardWidthPx / 2, love.graphics.getHeight() / 2 - cardHeightPx - cardSpacePx)
+    local card = nil
+    if #currentHand % 2 == 1 then
+        card = Cards[currentHand[idx].base][currentHand[idx].index]
+        renderCard(card, hW - cardWidthPx / 2, love.graphics.getHeight() / 2 - cardHeightPx - cardSpacePx)
         idx = idx + 1
 
-        if cardHand - 1 > 2 then
-            renderCard(hand[idx], hW - cardWidthPx / 2 - cardSpacePx - cardWidthPx, hH - cardHeightPx - cardSpacePx)
-            renderCard(hand[idx + 1], hW + cardWidthPx / 2 + cardSpacePx, hH - cardHeightPx - cardSpacePx)
-            idx = idx + 2
+        if #currentHand - 1 > 2 then
+            card = Cards[currentHand[idx].base][currentHand[idx].index]
+            renderCard(card, hW - cardWidthPx / 2 - cardSpacePx - cardWidthPx, hH - cardHeightPx - cardSpacePx)
+            idx = idx + 1
+
+            card = Cards[currentHand[idx].base][currentHand[idx].index]
+            renderCard(card, hW + cardWidthPx / 2 + cardSpacePx, hH - cardHeightPx - cardSpacePx)
+            idx = idx + 1
         end
     else
-        renderCard(hand[idx], hW - cardSpacePx / 2 - cardWidthPx, hH - cardHeightPx - cardSpacePx)
-        renderCard(hand[idx + 1], hW + cardSpacePx / 2, hH / 2 - cardHeightPx - cardSpacePx)
-        idx = idx + 2
+        card = Cards[currentHand[idx].base][currentHand[idx].index]
+        renderCard(card, hW - cardSpacePx / 2 - cardWidthPx, hH - cardHeightPx - cardSpacePx)
+        idx = idx + 1
+
+        card = Cards[currentHand[idx].base][currentHand[idx].index]
+        renderCard(card, hW + cardSpacePx / 2, hH / 2 - cardHeightPx - cardSpacePx)
+        idx = idx + 1
     end
 
-    renderCard(hand[idx], love.graphics.getWidth() / 2 - cardSpacePx / 2 - cardWidthPx, love.graphics.getHeight() / 2 + cardSpacePx)
-    renderCard(hand[idx + 1], love.graphics.getWidth() / 2 + cardSpacePx / 2, love.graphics.getHeight() / 2 + cardSpacePx, cardWidthPx)
+    card = Cards[currentHand[idx].base][currentHand[idx].index]
+    renderCard(card, love.graphics.getWidth() / 2 - cardSpacePx / 2 - cardWidthPx, love.graphics.getHeight() / 2 + cardSpacePx)
+    idx = idx + 1
+
+    card = Cards[currentHand[idx].base][currentHand[idx].index]
+    renderCard(card, love.graphics.getWidth() / 2 + cardSpacePx / 2, love.graphics.getHeight() / 2 + cardSpacePx, cardWidthPx)
 end
 
 local function renderTowers()
@@ -91,7 +97,7 @@ local function renderTowers()
 end
 
 function screen:onEnter()
-
+    cardDrawPlayer1()
 end
 
 function screen:onExit()
